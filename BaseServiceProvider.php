46be8ed8fc7582b1a6f233f0b189a1b3fab66333
sinan <d.sinan.k@gmail.com>
2022-10-12 16:47:06 +0300

    commit 8

--- BaseServiceProvider.php
+++ BaseServiceProvider.php
@@ -2,20 +2,6 @@
 
 namespace Nonoco\Base;
 
-use Illuminate\Support\ServiceProvider;
-use Nonoco\Base\Commands\Install;
-
-class BaseServiceProvider extends ServiceProvider
+class BaseServiceProvider
 {
-    public function register()
-    {
-        //
-    }
-
-    public function boot()
-    {
-        if ($this->app->runningInConsole()) {
-            $this->commands([Install::class]);
-        }
-    }
 }
